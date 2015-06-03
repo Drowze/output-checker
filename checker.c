7fc2a7cb407b1b3a52e6a2b0765491742100e2fb
U-giba-desktop\giba <gibim6@gmail.com>
2015-06-03 00:39:50 -0300

    Constant, not variable

--- checker.c
+++ checker.c
@@ -3,7 +3,7 @@
 
 // Instructions: All the .c files outputs shall only print an integer variable and a linebreak (\n)  
 //               All the .c files should be in the same directory of this file compiled AND the "input" file (which should contain all the desirable tests);  
-//               The desirable outputs shall be written in the expected_outputs vector; the number of desired outputs should also be placed in its variable
+//               The desirable outputs shall be written in the expected_outputs vector; the number of desired outputs should also be placed in its constant
 
 // This project also includes an example "inputs" file and suited file. Here's a sample command:
 // gcc foo.c -o bar.out && ./bar.out < input > output && gcc checker.c -o checker.out && ./checker.out
